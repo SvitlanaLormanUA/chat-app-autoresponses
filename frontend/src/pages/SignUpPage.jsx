c5a84922c9d902019bf44e334c4f779cf83e8e26
Svitlana Lorman <svlormanua@gmail.com>
2024-12-04 00:09:29 +0200

    started styling

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,27 @@
-
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
 const SignUpPage = () => {
-    return <></>;
+     const [showPassword, setShowPassword] = useState(false);
+     const [formData, setFormData] = useState({ 
+           fullname: "",
+            email: "",
+            password: "",
+            
+        });
+
+    const { signup, isSigningup } = useAuthStore();
+    const validateForm = () => {
+       
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+       
+    }
+    return <>
+    <div className="main-signup-page">
+            <div className="right-section"></div>
+            <div className="left-section"></div>
+    </div>
+    </>;
    };
    export default SignUpPage;

6ab6d45a4b868a9dee34178e0ff27ee62082ba20
Svitlana Lorman <svlormanua@gmail.com>
2024-12-05 17:51:37 +0200

    set up profile page

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); 
+
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,91 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Camera, Mail, User } from "lucide-react";
 
 const ProfilePage = () => {
-    return <></>;
-   };
-   export default ProfilePage;
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+  const [selectedImg, setSelectedImg] = useState(null);
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      setSelectedImg(base64Image);
+      await updateProfile({ profilePic: base64Image });
+    };
+  };
+
+  return (
+    <div className="profile-container">
+      <div className="profile-card">
+        <div className="text-center">
+          <h1 className="text-2xl font-semibold">Profile</h1>
+          <p className="mt-2">Your profile information</p>
+        </div>
+
+        <div className="avatar-upload">
+          <div className="avatar-wrapper">
+            <img
+              src={selectedImg || authUser.profilePic || "frontend/public/avatar.png"}
+              alt="Profile"
+              className="avatar"
+            />
+            <label
+              htmlFor="avatar-upload"
+              className={`upload-button ${isUpdatingProfile ? "loading" : ""}`}
+            >
+              <Camera className="icon" />
+              <input
+                type="file"
+                id="avatar-upload"
+                className="hidden"
+                accept="image/*"
+                onChange={handleImageUpload}
+                disabled={isUpdatingProfile}
+              />
+            </label>
+          </div>
+        </div>
+
+        <div className="profile-info">
+          <div className="profile-item">
+            <div className="profile-label">
+              <User className="icon" />
+              Full Name
+            </div>
+            <p className="profile-value">{authUser?.fullName}</p>
+          </div>
+
+          <div className="profile-item">
+            <div className="profile-label">
+              <Mail className="icon" />
+              Email Address
+            </div>
+            <p className="profile-value">{authUser?.email}</p>
+          </div>
+        </div>
+
+        {/* Account Information */}
+        <div className="account-info">
+          <h2 className="section-title">Account Information</h2>
+          <div className="account-item">
+            <span>Member Since</span>
+            <span>{authUser.createdAt?.split("T")[0]}</span>
+          </div>
+          <div className="account-item">
+            <span>Account Status</span>
+            <span className="active-status">Active</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
